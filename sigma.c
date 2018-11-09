daecbedeb121a2c9347233eb04d2ff689088b30a
Theodore Peters <tpeters@stuy.edu>
2018-11-08 20:43:04 -0500

    woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- sigma.c
+++ sigma.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 static void sighandler(int signo) {
-  if (signo==SIGINT) {
+  if (signo==SIGINT){
     printf("\nwow, it seems that SIGINT got hit.\n");
+    int copy = open("closure.txt",O_WRONLY|O_CREAT,0644);
+    write(copy, "file closed with SIGINT\n", 25);
+    close(copy);
     exit(EXIT_SUCCESS);
   } else if (signo==SIGUSR1) {
-    printf("my daddy's pid is %d\n", getppid());
+    printf("my daddy's pid is: %d\n", getppid());
   }
 }
 
@@ -18,7 +23,7 @@ int main() {
   signal(SIGUSR1, sighandler);
   
   while (1) {
-    printf("my pid be: %d\n", getpid());
+    printf("my pid is: %d\n", getpid());
     sleep(1);
   }
   return 0;
